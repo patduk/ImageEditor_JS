7613d515aa3f2cab2829ffe89c2920bec9544cbf
Paterosa <patrickdelarosa42@gmail.com>
2021-07-21 23:21:38 -0700

    c

--- scripts/ftn_oilpaint_old.js
+++ scripts/ftn_oilpaint_old.js
@@ -1,47 +1,43 @@
 
 //stolen from stackoverflow https://stackoverflow.com/questions/24222556/apply-a-oil-paint-sketch-effect-to-a-photo-using-javascript 
-function oilpaint(radius, intensity, is_standalone_filter) {
+function oilpaint(radius, intensity) {
 
-    //prep canvas and ctx (idk why its needed)
     let canvas = document.getElementById('cv2'); 
     let ctx = canvas.getContext('2d');
-    //image = new Image();
-    //ctx.drawImage(image, 0, 0);
 
-    if (is_standalone_filter === true) {
-        imageData = ctx.getImageData(0, 0, image.width, image.height);
+    ////1.0-1.4 store to undolist
+    ClearRedo();                   //0.8
+    is_FilterIncremental = false;   //0.9 //might be true to avoid playing flatten() in infinite loop
+    SaveAttributesToUndoLists();   //1-1.4
+    logprint();
     
-        ////1.0-1.4 store to undolist
-        ClearRedo();                   //0.8
-        is_FilterIncremental = false;   //0.9 //might be true to avoid playing flatten() in infinite loop
-        SaveAttributesToUndoLists();   //1-1.4
-        logprint();
-    
-    
-        ////2.0 (reset incremental filter attributes when user uses non-incremental filter)
-        for (key in DictV) { 
-            DictV[key] = 0;
-        }
+    ////2.0 (reset incremental filter attributes when user uses non-incremental filter)
+    for (key in DictV) { 
+        DictV[key] = 0;
     }
 
-    ////3.0 edit?
-    let width = canvas.width,
+
+    /////
+    /////
+
+
+    var width = canvas.width,
         height = canvas.height,
-        // imageData = ctx.getImageData(0, 0, width, height),
-        //pixData = imageData.data,
-        // destCanvas = document.createElement("canvas"),
-        // dCtx = destCanvas.getContext("2d"),
+        imgData = ctx.getImageData(0, 0, width, height),
+        pixData = imgData.data,
+        destCanvas = document.createElement("canvas"),
+        dCtx = destCanvas.getContext("2d"),
         pixelIntensityCount = [];
 
-    // destCanvas.width = width;
-    // destCanvas.height = height;
+    destCanvas.width = width;
+    destCanvas.height = height;
 
     // for demo purposes, remove this to modify the original canvas
-    //document.body.appendChild(destCanvas);
+    document.body.appendChild(destCanvas);
 
-    // var destImageData = dCtx.createImageData(width, height),
-    //     destPixData = destImageData.data;
-    var intensityLUT = [],
+    var destImageData = dCtx.createImageData(width, height),
+        destPixData = destImageData.data,
+        intensityLUT = [],
         rgbLUT = [];
 
     for (var y = 0; y < height; y++) {
@@ -49,9 +45,9 @@ function oilpaint(radius, intensity, is_standalone_filter) {
         rgbLUT[y] = [];
         for (var x = 0; x < width; x++) {
             var idx = (y * width + x) * 4,
-                r = imageData.data[idx],
-                g = imageData.data[idx + 1],
-                b = imageData.data[idx + 2],
+                r = pixData[idx],
+                g = pixData[idx + 1],
+                b = pixData[idx + 2],
                 avg = (r + g + b) / 3;
 
             intensityLUT[y][x] = Math.round((avg * intensity) / 255);
@@ -62,6 +58,7 @@ function oilpaint(radius, intensity, is_standalone_filter) {
             };
         }
     }
+
 
     for (y = 0; y < height; y++) {
         for (x = 0; x < width; x++) {
@@ -98,19 +95,21 @@ function oilpaint(radius, intensity, is_standalone_filter) {
             var curMax = pixelIntensityCount[0].val,
                 dIdx = (y * width + x) * 4;
 
-            imageData.data[dIdx] = ~~ (pixelIntensityCount[0].r / curMax);
-            imageData.data[dIdx + 1] = ~~ (pixelIntensityCount[0].g / curMax);
-            imageData.data[dIdx + 2] = ~~ (pixelIntensityCount[0].b / curMax);
-            imageData.data[dIdx + 3] = 255;
+            destPixData[dIdx] = ~~ (pixelIntensityCount[0].r / curMax);
+            destPixData[dIdx + 1] = ~~ (pixelIntensityCount[0].g / curMax);
+            destPixData[dIdx + 2] = ~~ (pixelIntensityCount[0].b / curMax);
+            destPixData[dIdx + 3] = 255;
         }
     }
 
-    if (is_standalone_filter === true) {
-        ////4.0 affix
-        //imageData_original2 = imageData;
-        Flatten_nosavingtoundo(); 
-    }
+    ////4.0 affix
+    //imageData_original2 = imageData;
+    Flatten_nosavingtoundo();
+
+    // change this to ctx to instead put the data on the original canvas
+    dCtx.putImageData(destImageData, 0, 0);
+    //OR
+    //ctx.putImageData(destImageData, 0, 0);
+
     
-    ////display new image
-    ctx.putImageData(imageData, 0, 0);  
 }
